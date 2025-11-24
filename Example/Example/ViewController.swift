5e75c94f6867eeeadb7032ee89bdf85752038e10
陈烁 <>
2025-11-24 11:13:07 +0800

    1.移除调试用的isAutoCloseController

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -237,7 +237,7 @@ class ViewController: UIViewController {
         
         /// Using this init method, you can continue editing the selected photo
         let ac = ZLPhotoPreviewSheet(results: takeSelectedAssetsSwitch.isOn ? selectedResults : nil)
-        ZLPhotoUIConfiguration.default().isAutoCloseController = false
+        
 //        let ac = ZLPhotoPreviewSheet(selectedAssets: takeSelectedAssetsSwitch.isOn ? selectedAssets : nil)
         
         ac.selectImageBlock = { [weak self] results, isOriginal in
