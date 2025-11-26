a4c41989a14358630e396d64561ac0b131e12f0b
陈烁 <>
2025-11-26 11:43:24 +0800

    no message

--- Sources/General/ZLPhotoPreviewSheet.swift
+++ Sources/General/ZLPhotoPreviewSheet.swift
@@ -620,7 +620,7 @@ public class ZLPhotoPreviewSheet: UIView {
             }
             
             /// 【Konvy注释】选择完照片是否关闭控制器
-            if ZLPhotoUIConfiguration.default().isAutoCloseController {
+            if self.isAutoCloseController {
                 if let vc = viewController {
                     self?.isHidden = true
                     self?.animate = false
